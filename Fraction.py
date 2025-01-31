3dbdc1d4906503b6618b9d0219ebf7df826acaf6
nico9696 <nicholasbenedictdysy@gmail.com>
2025-01-31 13:22:40 +0800

    added functionality for init function for if argument is string

--- Fraction.py
+++ Fraction.py
@@ -1,12 +1,26 @@
 class Fraction(object):
 
+    slash_reached = False
+    temp_numerator = ""
+    temp_denominator = ""
     def __init__(self, numerator=0, denominator=1):
         if isinstance(numerator, str):
             for i in numerator:
-                if numerator[i] == "-":
-                    temp_numeraotr = "-"
-                elif numerator[i] in "1234567890":
-                    continue
+                if i == "-":
+                    if slash_reached == False:
+                        temp_numerator = "-"
+                    else:
+                        temp_denominator = "-"
+                elif i in "1234567890":
+                    if slash_reached == False:
+                        temp_numerator += i
+                    else:
+                        temp_denominator += i
+                elif i == "/":
+                    slash_reached = True
+
+        numerator = int(temp_numerator)
+        denominator = int(temp_denominator)
 
     def gcd(a, b):
         #TODO
